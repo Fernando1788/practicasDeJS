9583d419da912d2895a5352a8b49c1f611987e7c
Fernando Dominguez <fernandodr1788@gmail.com>
2022-09-11 21:08:41 -0300

    Primer commit

---
+++ js/primeraClase.js
@@ -0,0 +1,30 @@
+/*console.log("hola mundo");
+Number(document.write("hola mundo)");
+console.log("este es un segundo mensaje");
+
+const nombre = prompt("Escriba un nombre");
+console.log(nombre);*/
+
+let suma = 0;
+
+let valorDeEntrada = Number(prompt("Indique un valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+valorDeEntrada = Number(prompt("Indique un valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+valorDeEntrada = Number(prompt("Indique un valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+valorDeEntrada = Number(prompt("Indique un valor"));
+console.log(valorDeEntrada);
+
+suma = suma + valorDeEntrada;
+
+console.log("La suma es de " + suma);
